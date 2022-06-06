101441bd5d1f9be83d0710efc91300afc86fc069
0102johnny <rt2824.recruit.1022@gmail.com>
2022-06-06 17:20:36 +0900

    47都道県のデータを管理するクラスファイルのインスタンスをViewController.Swift内で生成し、都道府県の数だけCellを表示させる処理の実装

--- JohnnyKadai10/ViewController.swift
+++ JohnnyKadai10/ViewController.swift
@@ -8,20 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let prefecturesDataManager = PrefecturesDataManager()
     @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
     }
+
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        6
+        prefecturesDataManager.dataArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomTableViewCell 
+        tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomTableViewCell
     }
 }
