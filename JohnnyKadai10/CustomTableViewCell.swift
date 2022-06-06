f9f3720113f084039fa73f98a1ce50b86989d276
0102johnny <rt2824.recruit.1022@gmail.com>
2022-06-06 16:49:02 +0900

    TableViewCellのクラスファイルを生成

---
+++ JohnnyKadai10/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  JohnnyKadai10
+//
+//  Created by Johnny Toda on 2022/06/06.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
